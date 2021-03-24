c7fef4f72169788ebacd45193f1f15ae803de47f
smsguy927 <smsguy927@gmail.com>
2021-03-24 17:37:11 -0500

    Done

---
+++ index.js
@@ -0,0 +1,33 @@
+function add (x, y) {
+    return x + y;
+}
+
+function subtract (x, y) {
+    return x - y;
+} 
+
+function multiply (x, y) {
+    return x * y;
+} 
+
+function divide (x, y) {
+    return x / y;
+} 
+
+function increment (x) {
+    x++;
+    return x;
+}
+
+function decrement (x) {
+    x--;
+    return x;
+}
+
+function makeInt (n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal (n) {
+    return parseFloat(n);
+}
